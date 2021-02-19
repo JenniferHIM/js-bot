450472cfa2150a6437e251f14a9b18c07e075332
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-02-19 12:16:50 +0200

    Added Module 1 tasks 1-36

--- js/hw6.js
+++ js/hw6.js
@@ -721,13 +721,13 @@
 // пол которых (свойство gender) совпадает со значением параметра gender.
 
 // Пиши код ниже этой строки
-const getTotalBalanceByGender = (users, gender) => {
+// const getTotalBalanceByGender = (users, gender) => {
     
-  return [...users]
-  .filter((user) => user.gender === gender)
-  .reduce((total, user) => {return total + user.balance
-   }, 0);
+//   return [...users]
+//   .filter((user) => user.gender === gender)
+//   .reduce((total, user) => {return total + user.balance
+//    }, 0);
    
-};
+// };
 // Пиши код выше этой строки
 
--- js/solo.js
+++ js/solo.js
@@ -43,3 +43,25 @@
 //      console.log(day);
 //     }
 
+
+
+// function test(number) {
+//     while (number < 5) {
+//         number +=;
+//     }
+//     return number;
+// }
+// alert(test(2)); // вернет 5
+
+
+function main() {
+let amount = parseFloat(readLine(), 10);
+let rate = parseFloat(readLine(), 10);
+
+console.log(convert(amount, rate));
+}
+
+function convert(a,b) {
+    return a * b;
+}
+console.log(convert(5, 10)); // вернет 50
